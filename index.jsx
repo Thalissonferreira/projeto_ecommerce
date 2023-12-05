87a79e90412ff8c49878d25ae2fff974e80d9b6e
Thalisson do Nascimento Ferreira <126815141+Thalissonferreira@users.noreply.github.com>
2023-12-04 22:03:12 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import './login.modules.css'
+import Footer from "../../components/Footer";
+import Cabecalho from "../../components/cabecalho";
+
+
+
+
+ function Login() {
+  const navigate = useNavigate();
+  const[ Email, setEmail]= useState("");
+  const[ Senha, setSenha]= useState("");
+  const [disable, setDisable] = useState(false);
+
+  const fazerLogin = async (e) => { 
+    e.preventDefault('');
+    setDisable(true);
+
+    const res = await fetch('',{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            email: Email,
+            senha: Senha
+        })
+    });
+ 
+
+      
+    if( res.status !== 200)
+      navigate("/Home")
+      if(res.status == 200){
+        alert("Login invalido!  Por favor tente novamente")
+      }
+  }
+ 
+
+      
+
+  return (
+
+<body>
+<Cabecalho/>
+    <section className='area-de-login'>
+
+        <form  className="form-login" onSubmit={fazerLogin}> 
+          <h2>LOGIN</h2>
+          <h4>Preencha seus dados de acesso</h4>
+            <fieldset className="inputs-login">
+              <section className="inputs-login">
+                <div className="img-input">
+                  <input  className='idEmail' type='email' placeholder='E-mail' onChange={e => setEmail(e.target.value)}/>
+                </div>
+                <div className="img-input">
+                  <input  className="idEmail" type='password' placeholder='Senha' onChange={e => setSenha(e.target.value)}/>
+                </div>
+                </section>
+            </fieldset>
+            <section className="area-btn-login">
+                <button disabled={disable} to={'/Home'} className="btn-login">Entrar</button>
+              </section>
+              <section>
+                <Link className="area-btn-login" to={'/Cadastro'}>Cadastre-se</Link>
+              </section>
+        </form>
+      </section>
+  <Footer/>
+ </body>
+
+
+
+  
+
+
+
+
+
+)
+  }
+  export default Login;
